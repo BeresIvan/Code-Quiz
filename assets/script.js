a1133e9bca939e26ecec48697d3e6c8796cdbd3b
GTIvan <ivberes@gmail.com>
2020-10-24 20:35:05 -0500

    added assets and css folder also added HTML files, css files js file and basic code

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+let statusSpan = document.querySelector("#status");
+let statusToggle = document.querySelector("#status-toggle");
+let startButton = document.querySelector("#start");
+let timeDisplay = document.querySelector("#time");
+let introCard = document.querySelector("#introcard");
+
+
+startButton.addEventListener("click", startQuiz);
+
+
+
+
+
+
+
+
+function startQuiz(){
+
+}
+
+
+
+
+function nextQuestion(){
+
+
+}
+
+
+function selectAnswer() {
+
+
+}
