9a47f9df9d046fdefb8132d8f33124300d15a4a1
GTIvan <ivberes@gmail.com>
2020-10-25 11:50:26 -0500

    added js coding cleaned up the files

--- HighScore.js
+++ HighScore.js
@@ -2,13 +2,24 @@ const username =document.getElementById("username");
 const submitBtn =document.getElementById("submitBtn");
 const finalScore =document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
-  submitBtn.disabled = !username.nodeValue;  
+  submitBtn.disabled = !username.Value;  
 });
 
 
 submitHighScore = e => {
-e.preventDefault();
+  e.preventDefault();
+
+  const score = {
+  score: mostRecentScore,
+  name: userName.value,
+  };
+  highScores.push(score);
+
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 };
