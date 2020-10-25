f194e207e0d4be237b70b87d1509d3819ce0c277
GTIvan <ivberes@gmail.com>
2020-10-25 00:08:25 -0500

    fixed typos in game.js

--- game.js
+++ game.js
@@ -85,43 +85,43 @@ let questions = [
 
 
 const CORRECT_BONUS = 10;
-const Max_QUESTIONS = 9;
+const MAX_QUESTIONS = 9;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
+    availableQuesions = [...questions];
     getNewQuestion();
 };
 
 getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(choice => {
+    choices.forEach((choice) => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
-  
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAmswers = true;
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
 };
 
-choices.forEach(choice => {
-  choice.addEventListener("click", e => {
-    if (!acceptingAnswers) return;
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
 
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
-
-    getNewQuestion();
-
-    
-  });
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
 });
 
 startGame();
